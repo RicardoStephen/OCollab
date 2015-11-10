034d37affb54c6de7223a8bbb609cb24eceba918
ak121 <ak633@cornell.edu>
2015-11-10 15:14:48 -0500

    Minor edits gui.mli

--- gui.mli
+++ gui.mli
@@ -1,20 +1,22 @@
-(* sending receiving things from server
-handling events like insertion/deletion on document
- *)
 
-(*stuff on the document*)
-(*Need to specify position. Should these return patch? or a pair of document and patch?*)
-val delete_text : document -> (*position?*) -> document * patch
-val insert_text : document -> (*position?*) -> document * patch
+type position
 
+(*Inserting or deleting text at certain position*)
+val delete_text : document -> position -> document * patch
+val insert_text : document -> position -> document * patch
 
-(*Don't need. THis is taken care of in document.ml*)
-val change_title : doc_id
+(*Finding search phrase within a given document*)
+val find : document -> string -> position
 
+(* Sending patches to server*)
+val patches_to_server : patch list -> unit
 
-(* maintaining current list of patches and merging with the server regularly *)
-val patches_to_server : (*might not need this, since it will just use current_patches_list*)patch list ->
-
+(* Not needed, done in document.mli
+(* List of patches for the document *)
 val current_patches_list : patch list
+*)
 
-
+(* Not needed, done with delete_text and insert_text
+(* Add a patch to a document *)
+val add_patch : patch list -> patch list
+*)

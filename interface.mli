83e655d1adb5a252d44d8bb895c002bb43530d40
RicardoStephen <ricardolstephen@gmail.com>
2015-11-12 18:25:55 -0500

    removed trash

--- interface.mli
+++ interface.mli
@@ -1,20 +1,3 @@
-(* Reference: https://ocsigen.org/tuto/manual/application
-              http://ocsigen.org/eliom/4.2/manual/server-services *)
-
-(* Register application *)
-(* main service should send gui js *)
-(* addtitional services *)
-(* - Send codemirror code *)
-(* - handle edit actions *)
-(* - handle update requests *)
-(* - handles initial document fetch. Should instantiate a new document if it does not
-     exist *)
-
-(* TODO This is what an elium applicaiton or service looks like (see below). Does it have
-   any place in a mli? Maybe this all should be part of the server.ml file, but inteface
-   allows the server to parse json from gui, do server-side manipulations on pathches,
-   and interface with the storage system? *)
-
 open Patch
 
 type selection
--- server.mli
+++ server.mli
@@ -10,6 +10,6 @@ val handle_get_doctext: doc_id -> response
 
 (* To handle the initial request, which will not have a any
  * patches. *)
-val handle_get_init: _ -> response
+val handle_get_init: unit -> response
 
 val get_document_text: doc_id -> document_text

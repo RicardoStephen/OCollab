cebaa3d44f5bfa94ec14f98335d45313bd4a78be
ak121 <ak633@cornell.edu>
2015-11-30 16:41:45 -0500

    Trying to get test_patch to compile

--- test_patch.ml
+++ test_patch.ml
@@ -1,5 +1,5 @@
 open Patch
-open Assertions
+(* open Assertions *)
 
 (*
 Properties to check for:
@@ -57,9 +57,9 @@ for i = 0 to num_edits do
   let size_patch_text = Random.int doc_size in
   let random_text = get_random_text size_patch_text in
   edits_array.(i) <- {op = patch_op; pos = patch_pos; text = random_text};
-done
-let edit_list = Array.to_list edits_array in
-edit_list
+done;
+ let edit_list = Array.to_list edits_array in
+ edit_list
 
 (*let doc_size = Random.int 2000 in
 let doc_text = get_random_text doc_size in
@@ -80,40 +80,40 @@ let edit_list = Array.to_list edits_array in
 (* compose edit_list (inverse edit_list) === empty_patch *)
 
 (* Empty patch does not change the document's text *)
-TEST_UNIT =
+TEST =
   let doc_size = Random.int 2000 in
-  let doc_text = get_random_text doc_size
+  let doc_text = get_random_text doc_size in
   let edit_list = [] in
-  apply_patch doc_text edit_list === doc_text
+  apply_patch doc_text edit_list = doc_text
 
 (* Applying patch composed with its inverse to empty document results in the empty document. *)
-TEST_UNIT =
+TEST =
   let edit_list = get_random_edit_list 0 in
   let doc_text = empty_doc (* will just be "" *)
-  apply_patch doc_text (compose edit_list (inverse edit_list)) === apply_patch doc_text empty_patch
+  apply_patch doc_text (compose edit_list (inverse edit_list)) = apply_patch doc_text empty_patch
 
 (* Applying patch composed with its inverse to non-empty document results in the original document text *)
-TEST_UNIT =
+TEST =
   let doc_size = Random.int 2000 in
   let edit_list = get_random_edit_list doc_size in
   let doc_text = get_random_text doc_size in
-  apply_patch doc_text (compose edit_list (inverse edit_list)) === apply_patch doc_text empty_patch
+  apply_patch doc_text (compose edit_list (inverse edit_list)) = apply_patch doc_text empty_patch
 
 (* Applying patch composed with another patch to an empty document is the same as applying the second patch
 to the empty document and then applying the first patch. *)
 (* TODO: is the order here correct? or should it be first patch, then second patch? *)
 (* TODO: haven't made sure that edit_list will have edits that are within the limits of the document text *)
-TEST_UNIT =
+TEST =
 let edit_list = get_random_edit_list () in
 let edit_list2 = get_random_edit_list () in
-apply_patch empty_doc (compose edit_list edit_list2) === apply_patch (apply_patch empty_doc edit_list2) edit_list
+apply_patch empty_doc (compose edit_list edit_list2) = apply_patch (apply_patch empty_doc edit_list2) edit_list
 
 (* Applying patch composed with another patch to a document is the same as applying the second patch to the
 document and then applying the first patch*)
 (* TODO: is the order here correct? or should it be first patch, then second patch? *)
 (* TODO: haven't made sure that edit_list will have edits that are within the limits of the document text *)
-TEST_UNIT =
+TEST =
 let edit_list = get_random_edit_list () in
 let edit_list2 = get_random_edit_list () in
-apply_patch empty_doc (compose edit_list edit_list2) === apply_patch (apply_patch empty_doc edit_list2) edit_list
+(apply_patch empty_doc (compose edit_list edit_list2)) = (apply_patch (apply_patch empty_doc edit_list2) edit_list)
 

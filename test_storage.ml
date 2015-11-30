995cc6f11e723c9b24f51afdff56d0aed8df9754
ak121 <ak633@cornell.edu>
2015-11-30 17:59:07 -0500

    Removed comments

--- test_storage.ml
+++ test_storage.ml
@@ -3,37 +3,10 @@ open Document
 open Storage
 open Redis
 
-(* main functions to test are
-set_document_text, add_document_patches, and their corresponding getters
-*)
-
-
-(*
-let get_random_patch () =
-let doc_size = Random.int 2000 in
-let doc_text = get_random_text doc_size in
-
-let num_edits = Random.int 50 in
-let edits_array = Array.make num_edits [] in
-for i = 0 to num_edits do
-  let patch_op = if Random.int 1 = 0 then Insert else Delete in
-  let patch_pos = Random.int doc_size in
-  let size_patch_text = Random.int doc_size in
-  let random_text = get_random_text size_patch_text in
-  edits_array.(i) <- {op = patch_op; pos = patch_pos; text = random_text};
-done
-(* let edit_list =  *)Array.to_list edits_array (* in *)
-(* edit_list *)
-*)
-
-
 let get_doc ctl =
   let opt = document_create ctl in
   (* Returns document's id *)
   match opt with Some d -> d | None -> "Failed to create doc"
-
-
-
 
  (* first test is to test whether connection was established *)
 let ctlopt = storage_open "127.0.0.1" 6379
@@ -49,10 +22,6 @@ TEST = set_document_text ctl doc_id "Lorem ipsum"
 TEST = get_document_text ctl doc_id = "Lorem ipsum"
 (* Ensure get_document_list is updating *)
 TEST = match get_document_list ctl with | [] -> false | h::t -> t = []
-
-(* don't need to test patch operations here -- taht will be tested in test_patch.  so...
-probably won't need to use get_random_patch *)
-
 
 let doc_id2 = get_doc ctl
 TEST = set_document_text ctl doc_id2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"

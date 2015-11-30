633d52fe81abc0b4af6b0ab270b5c8049071c26e
RicardoStephen <ricardolstephen@gmail.com>
2015-11-30 12:17:05 -0500

    handling basic doc req

--- editor.ml
+++ editor.ml
@@ -1,6 +1,8 @@
 open Eliom_lib
 open Eliom_content
+open Eliom_parameter
 open Storage
+open Eliom_content.Html5.D
 
 let ctl =
   match storage_open "127.0.0.1" 6379 with
@@ -17,3 +19,27 @@ let main_service =
     ~path:["create"]
     ~get_params:Eliom_parameter.any
     create_document
+
+let writeid id () =
+  Lwt.return 
+    Html5.D.(html
+               (head (title (pcdata "Hello")) [])
+               (body [p [pcdata "id was: ";
+                         strong [pcdata id];]])) 
+
+let idoc_service =
+  Eliom_registration.Html5.register_service
+    ~path:["doc"]
+    ~get_params:(string "id")
+    writeid
+
+(*let init_doc_access_service =
+  Eliom_registration.Html_text.register_service
+    ~path:["doc"]
+    ~get_params:(string "id")
+    (fun (id) () ->
+     Lwt.return Eliom_content.Html5.D.(
+       html
+         (head (title (pcdata "")) [])
+         (body
+            [p [pcdata "The current Document is"]]))) *)

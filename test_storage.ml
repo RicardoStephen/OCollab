cb69772c428c64e5691b0f411169cbdf007f56fb
ak121 <ak633@cornell.edu>
2015-12-01 13:38:16 -0500

    Added tests of applying patches to test_storage

--- test_storage.ml
+++ test_storage.ml
@@ -18,7 +18,7 @@ let ctl = match ctlopt with Some c -> c | None -> failwith "Failed to connect"
 (* Empty the database first *)
 TEST_UNIT = storage_flush ctl true
 
-TEST_UNIT = print_string ((string_of_int (List.length (get_document_list ctl))) ^ "\n\n\n");
+TEST_UNIT = (* print_string ((string_of_int (List.length (get_document_list ctl))) ^ "\n\n\n"); *)
 get_document_list ctl === []
 let doc_id = get_doc ctl
 TEST = set_document_text ctl doc_id "Lorem ipsum"
@@ -41,9 +41,46 @@ TEST_UNIT = match get_document_patches ctl doc_id 0 with
            (x:patch list) === [deletion]
        | None -> failwith ""
 
+TEST_UNIT = get_document_text ctl doc_id === Some "em ipsum"
+
+
+let insertion = [{op  = Insert; pos = 0; text = "Insertion: "}]
+TEST = add_document_patches ctl doc_id2 [insertion]
+TEST_UNIT = get_document_text ctl doc_id2 === Some "Insertion: ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+
+(* Assumption is that edit_list1 is done before edit_list2 *)
+let doc_id3 = get_doc ctl
+let doc_id4 = get_doc ctl
+TEST = set_document_text ctl doc_id2 "text"
+TEST = set_document_text ctl doc_id2 "text2"
+let edit_list1 = [{op = Delete; pos = 2; text = " "}; {op = Insert; pos = 3; text = "<inserted text>"}]
+let edit_list2 = [{op = Delete; pos = 8 text = "    "}]
+(* TODO: check order *)
+TEST = add_document_patches ctl doc_id3 [edit_list1]
+TEST_UNIT = get_document_text ctl doc_id3 === Some "tex<inserted text>"
+(* TODO: check order *)
+TEST = add_document_patches ctl doc_id4 [edit_list1; edit_list2]
+TEST_UNIT = get_document_text ctl doc_id4 === Some "tex<inse text>"
+
+
+
+(* Testing get_document_text gives None when text hasn't been set yet. *)
+let doc_id5 = get_doc ctl
+TEST_UNIT = get_document_text ctl doc_id5 === None
+
+(* Testing get_document_metadata gives None when metadata hasn't been set yet. *)
+TEST_UNIT = get_document_metadata ctl doc_id5 === None
+
+
+
+TEST_UNIT = get_document_list ctl === [doc_id; doc_id2; doc_id3; doc_id4; doc_id5]
+
+
 
 (* TODO: add test with adding two patch lists to document. Check order of patch lists *)
-
+(* Also check that text ends up as expected *)
 
 (* Setting document metadata *)
 TEST = set_document_metadata ctl doc_id {title = "This is a title"}

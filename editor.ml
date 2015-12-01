ec0726152a7482c9ffc758ccde1a13e85a8ea0b2
Scott Wu <scott.wu.hjx@gmail.com>
2015-11-30 22:34:08 -0500

    Testing

--- editor.ml
+++ editor.ml
@@ -1,6 +1,8 @@
 open Eliom_lib
 open Eliom_content
 open Eliom_parameter
+open Patch
+open Document
 open Storage
 open Eliom_content.Html5.D
 
@@ -9,37 +11,38 @@ let ctl =
   | Some x -> x
   | None -> failwith "Failed to connect to Redis"
 
+let clients = Hashtbl.create 20
+
+type session_id = string
+
+type client_info = {
+  (* Session id of the client - this is the hashtable key *)
+  sid : session_id;
+  (* Document id that the client is editing *)
+  docid : document_id;
+  (* Current patch that the client is on *)
+  patch_index : int;
+  (* Current queue of patches to be sent to the client *)
+  patch_queue : patch list
+}
+
 let create_document getp postp =
   match document_create ctl with
   | Some newid -> Lwt.return newid
   | None -> Lwt.return ""
 
-let main_service =
+let create_service =
   Eliom_registration.Html_text.register_service
     ~path:["create"]
     ~get_params:Eliom_parameter.any
     create_document
 
-let writeid id () =
-  Lwt.return 
-    Html5.D.(html
-               (head (title (pcdata "Hello")) [])
-               (body [p [pcdata "id was: ";
-                         strong [pcdata id];]])) 
+let create_session getp postp =
+  postp
+  let sid = String.init 16 (fun x -> Char.chr ((Random.int 26) + 65)) in
+  Lwt.return sid
 
-let idoc_service =
-  Eliom_registration.Html5.register_service
-    ~path:["doc"]
-    ~get_params:(string "id")
-    writeid
-
-(*let init_doc_access_service =
-  Eliom_registration.Html_text.register_service
-    ~path:["doc"]
-    ~get_params:(string "id")
-    (fun (id) () ->
-     Lwt.return Eliom_content.Html5.D.(
-       html
-         (head (title (pcdata "")) [])
-         (body
-            [p [pcdata "The current Document is"]]))) *)
+let session_service =
+  Eliom_registration.Html_text.register_post_service
+    ~path:["session"]
+    ~post_params:Eliom_parameter.any

7667b7d304686b0cc2af457197c6c6af5dfd52bd
Scott Wu <scott.wu.hjx@gmail.com>
2015-12-02 17:41:50 -0500

    Added to/from json for patches

--- patch.mli
+++ patch.mli
@@ -64,9 +64,19 @@ val merge : patch -> patch -> patch * patch
 val apply_patch : document_text -> patch -> document_text
 
 (*
+ * Converts a patch to a json representation.
+ *)
+val json_of_patch : patch -> Yojson.Basic.json
+
+(*
  * Converts a patch to a string representation.
  *)
 val string_of_patch : patch -> string
+
+(*
+ * Converts a string representation to a patch.
+ *)
+val patch_of_json : Yojson.Basic.json -> patch
 
 (*
  * Converts a string representation to a patch.
